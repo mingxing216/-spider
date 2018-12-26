25ae9eadbdece11531939973639a2d792229bad1
develop <>
2018-12-26 18:13:46 +0800

    企查查爬虫任务抓取爬虫增加无限循环

--- Project/QiChaCha/main/company_url_main.py
+++ Project/QiChaCha/main/company_url_main.py
@@ -33,56 +33,57 @@ class SpiderMain(object):
     def run(self):
         redis_client = redispool_utils.createRedisPool()
         mysql_client = mysqlpool_utils.createMysqlPool()
-        # 获取入口页响应
-        index_resp = self.download.getResp(logging=LOGGING, redis_client=redis_client, url=self.index_url)
-        # with open('index.html', 'r') as f:
-        #     index_resp = f.read()
+        while 1:
+            # 获取入口页响应
+            index_resp = self.download.getResp(logging=LOGGING, redis_client=redis_client, url=self.index_url)
+            # with open('index.html', 'r') as f:
+            #     index_resp = f.read()
 
-        # 获取地区分类url列表
-        add_url_list = self.server.getAddUrlList(logging=LOGGING, resp=index_resp)
+            # 获取地区分类url列表
+            add_url_list = self.server.getAddUrlList(logging=LOGGING, resp=index_resp)
 
-        # 获取行业分类url列表
-        industry_url_list = self.server.getIndustryUrlList(logging=LOGGING, resp=index_resp)
+            # 获取行业分类url列表
+            industry_url_list = self.server.getIndustryUrlList(logging=LOGGING, resp=index_resp)
 
-        '''处理地区分类'''
-        # # 获取cookie
-        # cookie_list = self.dao.getCookieList(logging=LOGGING, redis_client=redis_client)
-        for add_url in add_url_list:
-            # 限定最大翻页500页
-            for i in range(500):
-                url = re.sub(r"1\.html", "{}.html".format(i + 1), add_url)
-                # 获取当前页响应
-                page_resp = self.download.getResp(logging=LOGGING, redis_client=redis_client, url=url)
-                # 获取企业url列表
-                if page_resp is None:
-                    continue
-                company_url_list = self.server.getCompanyUrlList(logging=LOGGING, resp=page_resp)
-                # 判断是否到最后一页
-                if not company_url_list:
-                    break
-                # 保存企业url
-                self.dao.saveCompanyUrl(logging=LOGGING, mysql_client=mysql_client, data=company_url_list)
+            '''处理地区分类'''
+            # # 获取cookie
+            # cookie_list = self.dao.getCookieList(logging=LOGGING, redis_client=redis_client)
+            for add_url in add_url_list:
+                # 限定最大翻页500页
+                for i in range(500):
+                    url = re.sub(r"1\.html", "{}.html".format(i + 1), add_url)
+                    # 获取当前页响应
+                    page_resp = self.download.getResp(logging=LOGGING, redis_client=redis_client, url=url)
+                    # 获取企业url列表
+                    if page_resp is None:
+                        continue
+                    company_url_list = self.server.getCompanyUrlList(logging=LOGGING, resp=page_resp)
+                    # 判断是否到最后一页
+                    if not company_url_list:
+                        break
+                    # 保存企业url
+                    self.dao.saveCompanyUrl(logging=LOGGING, mysql_client=mysql_client, data=company_url_list)
 
-            time.sleep(random.randint(40, 60))
+                time.sleep(random.randint(40, 60))
 
-        '''处理行业分类'''
-        # for industry_url in industry_url_list:
-        #     # 限定最大翻页500页
-        #     for i in range(500):
-        #         url = re.sub(r"&p=\d+", "&p={}".format(i + 1), industry_url)
-        #         # 获取当前页响应
-        #         page_resp = self.download.getIndustryResp(logging=LOGGING, redis_client=redis_client, url=url)
-        #         if page_resp is None:
-        #             continue
-        #         # 获取企业url列表
-        #         company_url_list = self.server.getCompanyUrlList(logging=LOGGING, resp=page_resp)
-        #         # 判断是否到最后一页
-        #         if not company_url_list:
-        #             break
-        #         # 保存企业url
-        #         self.dao.saveCompanyUrl(logging=LOGGING, mysql_client=mysql_client, data=company_url_list)
-        #
-        #     time.sleep(random.randint(40, 60))
+            '''处理行业分类'''
+            # for industry_url in industry_url_list:
+            #     # 限定最大翻页500页
+            #     for i in range(500):
+            #         url = re.sub(r"&p=\d+", "&p={}".format(i + 1), industry_url)
+            #         # 获取当前页响应
+            #         page_resp = self.download.getIndustryResp(logging=LOGGING, redis_client=redis_client, url=url)
+            #         if page_resp is None:
+            #             continue
+            #         # 获取企业url列表
+            #         company_url_list = self.server.getCompanyUrlList(logging=LOGGING, resp=page_resp)
+            #         # 判断是否到最后一页
+            #         if not company_url_list:
+            #             break
+            #         # 保存企业url
+            #         self.dao.saveCompanyUrl(logging=LOGGING, mysql_client=mysql_client, data=company_url_list)
+            #
+            #     time.sleep(random.randint(40, 60))
 
 if __name__ == '__main__':
     cookies = 'null'
