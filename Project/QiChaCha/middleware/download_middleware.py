69010e18243e0db526e86584774834e7d1929109
wangyong <wangyong@onecooo.com>
2018-12-27 00:43:56 +0800

    框架添加http代理池和https代理池

--- Project/QiChaCha/middleware/download_middleware.py
+++ Project/QiChaCha/middleware/download_middleware.py
@@ -32,65 +32,65 @@ class Download(object):
     def getResp(self, logging, redis_client, url):
         for i in range(3):
             # 获取代理IP
-            proxies = proxy_utils.getProxy(redis_client=redis_client, logging=logging)
+            proxies = proxy_utils.getHttpsProxy(redis_client=redis_client, logging=logging)
             if proxies is None:
-                logging.error('代理获取失败')
+                # logging.error('代理获取失败')
                 continue
-            logging.info('代理获取成功: {}'.format(proxies))
+            # logging.info('代理获取成功: {}'.format(proxies))
 
             for down_num in range(2):
-                logging.info('发起请求第 {} 次: {}'.format(down_num + 1, url))
+                # logging.info('发起请求第 {} 次: {}'.format(down_num + 1, url))
                 # 获取入口页html响应
                 resp = downloader.newGetRespForGet(logging=logging, url=url, headers=self.headers, proxies=proxies)
                 if resp is None:
-                    logging.error('请求失败: {}'.format(url))
+                    # logging.error('请求失败: {}'.format(url))
                     continue
 
-                logging.info('请求成功: {}'.format(url))
+                # logging.info('请求成功: {}'.format(url))
                 return resp
 
-            logging.info('删除当前代理: {}'.format(proxies))
-            proxy_utils.delProxy(redis_client=redis_client, proxies=proxies)
-            logging.info('开始第 {} 次获取代理'.format(i + 2))
+            # logging.info('删除当前代理: {}'.format(proxies))
+            proxy_utils.delHttpsProxy(redis_client=redis_client, proxies=proxies)
+            # logging.info('开始第 {} 次获取代理'.format(i + 2))
 
-        logging.error('重复请求次数已达到最大值。')
+        # logging.error('重复请求次数已达到最大值。')
         return None
 
     # 获取当前页响应
     def getIndustryResp(self, logging, redis_client, url):
         for i in range(3):
             # 获取代理IP
-            proxies = proxy_utils.getLangProxy(redis_client=redis_client, logging=logging)
+            proxies = proxy_utils.getHttpsProxy(redis_client=redis_client, logging=logging)
             if proxies is None:
                 continue
 
             for down_num in range(2):
-                logging.info('发起请求第 {} 次: {}'.format(down_num + 1, url))
+                # logging.info('发起请求第 {} 次: {}'.format(down_num + 1, url))
                 # 获取入口页html响应
                 resp = downloader.newGetRespForGet(logging=logging, url=url, headers=self.headers, proxies=proxies)
                 if resp is None:
-                    logging.error('请求失败: {}'.format(url))
+                    # logging.error('请求失败: {}'.format(url))
                     continue
 
-                logging.info('请求成功: {}'.format(url))
+                # logging.info('请求成功: {}'.format(url))
                 return resp
 
-            logging.info('开始第 {} 次获取代理'.format(i + 2))
+            # logging.info('开始第 {} 次获取代理'.format(i + 2))
 
-        logging.error('重复请求次数已达到最大值。')
+        # logging.error('重复请求次数已达到最大值。')
         return None
 
     # 获取机构主页响应
     def getJiGouHtml(self, logging, url, proxies):
         for down_num in range(2):
-            logging.info('发起请求第 {} 次: {}'.format(down_num + 1, url))
+            # logging.info('发起请求第 {} 次: {}'.format(down_num + 1, url))
             # 获取入口页html响应
             resp = downloader.newGetRespForGet(logging=logging, url=url, headers=self.headers, proxies=proxies)
             if resp is None:
-                logging.error('请求失败: {}'.format(url))
+                # logging.error('请求失败: {}'.format(url))
                 continue
             else:
-                logging.info('请求成功: {}'.format(url))
+                # logging.info('请求成功: {}'.format(url))
                 return resp
 
         return None
