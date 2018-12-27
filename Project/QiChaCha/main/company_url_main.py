ddb80ccc076c8b8f9cdd1f40b65e0b5333ab8af3
develop <>
2018-12-27 23:05:41 +0800

    修改企查查任务抓取逻辑

--- Project/QiChaCha/main/company_url_main.py
+++ Project/QiChaCha/main/company_url_main.py
@@ -31,16 +31,16 @@ class SpiderMain(object):
         self.dao = dao.Dao()
 
     def run(self):
-        redis_client = redispool_utils.createRedisPool()
         mysql_client = mysqlpool_utils.createMysqlPool()
         while 1:
             # 获取入口页响应
-            index_resp = self.download.getResp(logging=LOGGING, redis_client=redis_client, url=self.index_url)
-            if not index_resp:
-                LOGGING.error('入口页响应获取失败')
+            index_resp = self.download.getResp_Adsl(logging=LOGGING, url=self.index_url)
+            if index_resp['status'] == 0:
+                index_resp = index_resp['data']
+                LOGGING.info('入口页响应获取成功')
+            else:
+                LOGGING.info('入口页响应获取失败')
                 continue
-
-            LOGGING.info('入口页响应获取成功')
 
             # 获取地区分类url列表
             add_url_list = self.server.getAddUrlList(logging=LOGGING, resp=index_resp)
@@ -53,44 +53,59 @@ class SpiderMain(object):
                 # 限定最大翻页500页
                 for i in range(500):
                     url = re.sub(r"1\.html", "{}.html".format(i + 1), add_url)
-                    # 获取当前页响应
-                    page_resp = self.download.getResp(logging=LOGGING, redis_client=redis_client, url=url)
-                    # 获取企业url列表
-                    if page_resp is None:
+                    while 1:
+                        # 获取当前页响应
+                        page_resp = self.download.getResp_Adsl(logging=LOGGING, url=url)
+                        # 获取企业url列表
+                        if page_resp['status'] == 1:
+                            LOGGING.error('当前页获取失败')
+                            page_resp = None
+                            break
+                        if page_resp['status'] == 2:
+                            continue
+                        else:
+                            page_resp = page_resp['data']
+                            break
+
+                    if not page_resp:
                         continue
-                    # 判断是否出现验证码
-                    if len(page_resp) < 200:
-                        LOGGING.error('出现验证码')
-                        continue
+
                     company_url_list = self.server.getCompanyUrlList(logging=LOGGING, resp=page_resp)
                     # 判断是否到最后一页
                     if not company_url_list:
                         break
                     # 保存企业url
                     self.dao.saveCompanyUrl(logging=LOGGING, mysql_client=mysql_client, data=company_url_list)
-
 
             '''处理行业分类'''
             for industry_url in industry_url_list:
                 # 限定最大翻页500页
                 for i in range(500):
                     url = re.sub(r"&p=\d+", "&p={}".format(i + 1), industry_url)
-                    # 获取当前页响应
-                    page_resp = self.download.getIndustryResp(logging=LOGGING, redis_client=redis_client, url=url)
-                    if page_resp is None:
+                    while 1:
+                        # 获取当前页响应
+                        page_resp = self.download.getResp_Adsl(logging=LOGGING, url=url)
+                        # 获取企业url列表
+                        if page_resp['status'] == 1:
+                            LOGGING.error('当前页获取失败')
+                            page_resp = None
+                            break
+                        if page_resp['status'] == 2:
+                            continue
+                        else:
+                            page_resp = page_resp['data']
+                            break
+
+                    if not page_resp:
                         continue
-                    # 判断是否出现验证码
-                    if len(page_resp) < 200:
-                        LOGGING.error('出现验证码')
-                        continue
-                    # 获取企业url列表
+
                     company_url_list = self.server.getCompanyUrlList(logging=LOGGING, resp=page_resp)
                     # 判断是否到最后一页
                     if not company_url_list:
                         break
                     # 保存企业url
                     self.dao.saveCompanyUrl(logging=LOGGING, mysql_client=mysql_client, data=company_url_list)
-
+            # break
 
 
 if __name__ == '__main__':
