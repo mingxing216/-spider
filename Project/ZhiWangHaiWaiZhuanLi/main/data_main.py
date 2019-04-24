b4feb25d72b1dbd6fc865a67c54f2ff82c633c6d
develop <>
2019-04-24 10:17:29 +0800

    测试

--- Project/ZhiWangHaiWaiZhuanLi/main/data_main.py
+++ Project/ZhiWangHaiWaiZhuanLi/main/data_main.py
@@ -185,11 +185,13 @@ class SpiderMain(BastSpiderMain):
         #     # 获取当前页显示的所有同族专利
         #     self.server.getTzzl1(resp=tzzl_index_response, save_data=save_data)
 
-        status = self.dao.saveDataToHbase(data=save_data)
-        LOGGING.info(status)
+        print(save_data)
 
-        # 删除任务
-        self.dao.deleteTask(table=config.MYSQL_TASK, url=url)
+        # status = self.dao.saveDataToHbase(data=save_data)
+        # LOGGING.info(status)
+        #
+        # # 删除任务
+        # self.dao.deleteTask(table=config.MYSQL_TASK, url=url)
 
     def start(self):
         while 1:
