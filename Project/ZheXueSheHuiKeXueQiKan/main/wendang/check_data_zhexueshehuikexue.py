f34a1fb9e9547de87849b2baf201b4b012c2099f
develop <>
2020-11-04 20:13:27 +0800

    判断hbase数据库中全文是否正确

---
+++ Project/ZheXueSheHuiKeXueQiKan/main/wendang/check_data_zhexueshehuikexue.py
@@ -0,0 +1,206 @@
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+import hashlib
+import json
+import random
+import re
+import time
+import traceback
+
+# import gevent
+# from gevent import monkey
+# monkey.patch_all()
+# from contextlib import closing
+from io import BytesIO
+from multiprocessing.pool import Pool, ThreadPool
+from PyPDF2 import PdfFileReader
+from loguru import logger
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "../..")))
+
+from Project.ZheXueSheHuiKeXueQiKan.middleware import download_middleware
+from Project.ZheXueSheHuiKeXueQiKan.service import service
+from Project.ZheXueSheHuiKeXueQiKan.dao import dao
+from Project.ZheXueSheHuiKeXueQiKan import config
+from settings import DOWNLOAD_MIN_DELAY, DOWNLOAD_MAX_DELAY
+from Utils import user_pool, hbase_pool
+
+logger_format = "{time:YYYY-MM-DD HH:mm:ss.SSS} {process} {thread} {level} - {message}"
+
+# 输出到指定目录下的log文件，并按天分隔
+logger.add("/opt/Log/SheHuiKeXue/全文_check_{time}.log",
+           format=logger_format,
+           level="INFO",
+           rotation='00:00',
+           # compression='zip',
+           enqueue=True,
+           encoding="utf-8")
+
+
+class BastSpiderMain(object):
+    def __init__(self):
+        # cookie池
+        self.cookie_obj = user_pool.CookieUtils(logging=logger)
+        # 下载中间件
+        self.download_middleware = download_middleware.Downloader(logging=logger,
+                                                                  proxy_type=config.PROXY_TYPE,
+                                                                  stream=config.STREAM,
+                                                                  timeout=config.TIMEOUT,
+                                                                  proxy_country=config.COUNTRY,
+                                                                  proxy_city=config.CITY,
+                                                                  cookie_obj=self.cookie_obj)
+        # self.server = service.Server(logging=LOGGING)
+        # 存储
+        self.dao = dao.Dao(logging=logger,
+                           mysqlpool_number=config.MYSQL_POOL_NUMBER,
+                           redispool_number=config.REDIS_POOL_NUMBER)
+        # hbase存储对象
+        self.hbase_obj = hbase_pool.HBasePool()
+
+
+class SpiderMain(BastSpiderMain):
+    def __init__(self):
+        super().__init__()
+        self.num = 0
+
+    def __get_resp(self, url, method, s=None, data=None, cookies=None, referer=None, ranges=None, user=None):
+        """
+
+        :type referer: basestring
+        """
+        # 发现验证码，请求页面3次
+        for _i in range(3):
+            resp = self.download_middleware.getResp(url, method, s=s, data=data,
+                                                    cookies=cookies, referer=referer, ranges=ranges, user=user)
+            # if resp and resp.headers['Content-Type'].startswith('text'):
+            #     if '请输入验证码' in resp.text:
+            #         print('请重新登录')
+            #         LOGGING.error('出现验证码: {}'.format(url))
+            #         continue
+            return resp
+        else:
+            return
+
+    # 检测PDF文件正确性
+    @staticmethod
+    def is_valid_pdf_bytes_io(content):
+        b_valid = True
+        try:
+            reader = PdfFileReader(BytesIO(content))
+            if reader.getNumPages() < 1:  # 进一步通过页数判断。
+                b_valid = False
+        except:
+            b_valid = False
+
+        return b_valid
+
+    def handle(self, task_data):
+        # 判断是否有全文url
+        pdf_url = task_data.get('pdfUrl')
+        if pdf_url:
+            pdf_sha = hashlib.sha1(pdf_url.encode('utf-8')).hexdigest()
+            _id = task_data.get('id')
+            paper_url = task_data.get('url')
+            paper_sha = task_data.get('sha')
+
+            # 获取hbase中全文数据
+            info = self.hbase_obj.get_datas(pdf_sha).decode('utf-8')
+            print(info)
+
+            # 检测PDF文件
+            is_value = self.is_valid_pdf_bytes_io(info)
+            if not is_value:
+                # 更新全文种子错误信息及状态
+                msg = 'not PDF/url: {}'.format(pdf_url)
+                logger.warning(msg)
+                # data_dict = {'url': paper_url}
+                # self.dao.saveTaskToMysql(table=config.MYSQL_PAPER, memo=data_dict, ws='国家哲学社会科学', es='期刊论文', msg=msg)
+            else:
+                # 记录全文正确信息
+                msg = 'is PDF/url: {}'.format(pdf_url)
+                logger.info(msg)
+
+        else:
+            logger.info('无全文数据')
+
+    def run(self):
+        logger.info('线程启动')
+        # 第一次请求的等待时间
+        delay_time = time.time()
+        time.sleep(random.uniform(DOWNLOAD_MIN_DELAY, DOWNLOAD_MAX_DELAY))
+        logger.info('handle | download delay | use time: {}'.format('%.3f' % (time.time() - delay_time)))
+        # 单线程无限循环
+        while True:
+            # 获取任务
+            start_time = time.time()
+            # task = self.dao.get_one_task(key=config.REDIS_ZHEXUESHEHUIKEXUE_PAPER)
+            task = '{"url": "http://www.nssd.org/articles/article_detail.aspx?id=12165488", ' \
+                   '"authors": "鲁歌|刘娜", ' \
+                   '"pdfUrl": "http://www.nssd.org/articles/article_down.aspx?id=12165488", ' \
+                   '"id": "12165488", ' \
+                   '"qikanUrl": "http://www.nssd.org/journal/cn/96698B/", ' \
+                   '"xuekeleibie": "文化科学", ' \
+                   '"year": "1992", "issue": "03", ' \
+                   '"sha": "0007c10a1b210642cfa783c6cf67d076570535aa", ' \
+                   '"title": "《金瓶梅》作者是贾梦龙吗？"}'
+            if task:
+                try:
+                    # json数据类型转换
+                    task_data = json.loads(task)
+                    # 获取全文数据并判断是否正确
+                    self.handle(task_data=task_data)
+
+                    logger.info('handle | task complete | use time: {}'.format('%.3f' % (time.time() - start_time)))
+
+                except:
+                    logger.exception(str(traceback.format_exc()))
+                    logger.info('handle | task complete | use time: {}'.format('%.3f' % (time.time() - start_time)))
+            else:
+                time.sleep(1)
+                logger.info('handle | task complete | use time: {}'.format('%.3f' % (time.time() - start_time)))
+
+    def start(self):
+        # gevent.joinall([gevent.spawn(self.run, task) for task in task_list])
+
+        # # 创建gevent协程
+        # g_list = []
+        # for i in range(8):
+        #     s = gevent.spawn(self.run)
+        #     g_list.append(s)
+        # gevent.joinall(g_list)
+
+        self.run()
+
+        # # 创建线程池
+        # thread_pool = ThreadPool(processes=config.THREAD_NUM)
+        # for thread_index in range(config.THREAD_NUM):
+        #     thread_pool.apply_async(func=self.run)
+        #
+        # thread_pool.close()
+        # thread_pool.join()
+
+
+def process_start():
+    main = SpiderMain()
+    try:
+        main.start()
+        # main.run(task="{'id': '207d122a-3a68-41b1-8d8a-f20552f22054', 'xueKeLeiBie': '信息科学部', 'url': 'http://ir.nsfc.gov.cn/paperDetail/207d122a-3a68-41b1-8d8a-f20552f22054'}")
+    except:
+        logger.exception(str(traceback.format_exc()))
+
+
+if __name__ == '__main__':
+    logger.info('======The Start!======')
+    begin_time = time.time()
+    process_start()
+    # 创建多进程
+    # po = Pool(processes=config.PROCESS_NUM)
+    # for _count in range(config.PROCESS_NUM):
+    #     po.apply_async(func=process_start)
+    # po.close()
+    # po.join()
+    end_time = time.time()
+    logger.info('======The End!======')
+    logger.info('====== Time consuming is %.3fs ======' % (end_time - begin_time))
--- Utils/hbase_pool.py
+++ Utils/hbase_pool.py
@@ -7,135 +7,36 @@ import sys
 import os
 import happybase
 
-sys.path.append(os.path.dirname(__file__) + os.sep + "../")
-import settings
+# sys.path.append(os.path.dirname(__file__) + os.sep + "../")
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "../")))
 
 
 class HBasePool(object):
     def __init__(self):
         # 创建连接，通过参数size来设置连接池中连接的个数
         self.pool = happybase.ConnectionPool(size=5, # 连接池中连接个数
-                                             host="60.195.249.127",
-                                             # port=10000,
-                                             timeout=20,
-                                             autoconnect=True # 连接是否直接打开
+                                             host="mm-node5",
+                                             # port=9090,
+                                             # timeout=20,
+                                             autoconnect=True, # 连接是否直接打开
                                              # table_prefix=None, # 用于构造表名的前缀
                                              # table_prefix_separator=b'_', # 用于table_prefix的分隔符
                                              # compat='0.98', # 兼容模式
-                                             # transport='buffered', # 运输模式
-                                             # protocol='binary' # 协议
+                                             transport='framed', # 运输模式
+                                             protocol='compact' # 协议
                                             )
 
-    def _do_in_cursor(self, callback, pool):
-        conn = None
-        try:
-            conn = pool.connection()
-            cursor = conn.cursor()
-            result = callback(cursor)
-            conn.commit()
-            cursor.close()
-            return result
-
-        finally:
-            if conn:
-                conn.close()
-
-    # 获取连接
-    def get_connection(self):
+    # 获取一行数据
+    def get_datas(self, row_key):
         with self.pool.connection() as connection:
-            print(connection.tables())
+            # print(connection.tables())
+            table = happybase.Table('media:document', connection)
+            # print(table.families())
+            info = table.row(row_key, columns=None, include_timestamp=False)
+            return info
 
-    # 执行一条sql语句
-    def execute(self, sql):
-        def run(cursor):
-            try:
-                res = cursor.execute(sql)
-                return res
-            except:
-
-                return None
-
-        return self._do_in_cursor(run, self.pool)
-
-    # 查询所有结果
-    def get_results(self, sql):
-        '''
-        :param sql: sql查询语句
-        :return: [{}, {}, {}...]
-        '''
-        def run(cursor):
-            try:
-                cursor.execute(sql)
-                res = cursor.fetchall()
-                return res
-            except:
-
-                return []
-
-        return self._do_in_cursor(run, self.pool)
-
-    # 查询一个结果
-    def get_result(self, sql):
-        '''
-        :param sql: sql查询语句
-        :return: {}
-        '''
-        def run(cursor):
-            try:
-                cursor.execute(sql)
-                res = cursor.fetchone()
-                return res
-            except:
-
-                return {}
-
-        return self._do_in_cursor(run, self.pool)
-
-    # 插入一条数据
-    def insert_one(self, table, data):
-        '''
-        :param table: 表名
-        :param data: 要插入的数据｛key: value｝
-        '''
-        def run(cursor):
-            name = []
-            value = []
-
-            for key in data:
-                name.append(key)
-                value.append(data[key])
-
-            sql = "insert into {table}(`{name}`) VALUES ('{value}')".format(table='`' + table + '`',
-                                                                            name='`,`'.join(str(n) for n in name),
-                                                                            value="','".join(pymysql.escape_string(str(v)) for v in value))
-            # print(sql)
-
-            cursor.execute(sql)
-
-        return self._do_in_cursor(run, self.pool)
-
-    # 更新数据
-    def update(self, table, data, where):
-        '''
-        :param table: 表名
-        :param data: 要修改的数据　{key: value}
-        :param where: 判断语句
-        '''
-        def run(cursor):
-            updates = []
-
-            for key in data:
-                index = "{}='{}'".format(key, pymysql.escape_string(data[key])) # 存数据库时，单双引号反斜杠转义，防止存储失败
-                updates.append(index)
-
-            sql = "update `{}` set {} WHERE {}".format(table, ','.join(updates), where)
-            # print(sql)
-
-            cursor.execute(sql)
-
-        return self._do_in_cursor(run, self.pool)
 
 if __name__ == '__main__':
     hb = HBasePool()
-    hb.get_connection()
+    hb.get_datas('ef4c10c09147242b65750a77c2b8d28d8a579147')
 
